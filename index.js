7d4b39f570fe1f3f27dc5d06718f64b8512850f1
Carlos Pereira <carlos.dev.pereira@gmail.com>
2022-09-27 23:32:51 +0100

    chore: refactor code to reuse results from head commit

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-import fs from "fs";
-import core from "@actions/core";
-import github from "@actions/github";
-import { exec } from "@actions/exec";
-import shellac from "shellac";
+import fs from 'fs'
+import { HttpClient } from '@actions/http-client'
+import github from '@actions/github'
+import core from '@actions/core'
+import { exec } from '@actions/exec'
+import shellac from 'shellac'
 
-const SUPPORTED_TEST_FRAMEWORKS = ["jest"];
-const COVERAGE_OUTPUT_FOLDER = "./coverage";
+const SUPPORTED_TEST_FRAMEWORKS = ['jest']
+const COVERAGE_OUTPUT_FOLDER = './coverage';
 
 // 🚀 Execute Github Action
 (async () => {
@@ -15,20 +16,14 @@ const COVERAGE_OUTPUT_FOLDER = "./coverage";
      * the Github Action so that we can later use them
      **/
     const input = {
-      framework: core.getInput("framework"),
-      githubToken: core.getInput("githubToken", { required: true }),
-      branchName: core.getInput("branchName", { required: true }),
-      cloudflareProjectName: core.getInput("cloudflareProjectName", {
-        required: true,
-      }),
-      cloudflareApiToken: core.getInput("cloudflareApiToken", {
-        required: true,
-      }),
-      cloudflareAccountId: core.getInput("cloudflareAccountId", {
-        required: true,
-      }),
-      baseCloudflareDeploymentUrl: core.getInput("baseCloudflareDeploymentUrl"),
-    };
+      framework: core.getInput('framework'),
+      githubToken: core.getInput('githubToken', { required: true }),
+      branchName: core.getInput('branchName', { required: true }),
+      cloudflareProjectName: core.getInput('cloudflareProjectName', { required: true }),
+      cloudflareApiToken: core.getInput('cloudflareApiToken', { required: true }),
+      cloudflareAccountId: core.getInput('cloudflareAccountId', { required: true }),
+      baseCloudflareDeploymentUrl: core.getInput('baseCloudflareDeploymentUrl')
+    }
 
     /**
      * Check if requested test framework is supported
@@ -38,9 +33,8 @@ const COVERAGE_OUTPUT_FOLDER = "./coverage";
      **/
     if (!SUPPORTED_TEST_FRAMEWORKS.includes(input.framework)) {
       throw new Error(
-        "Unsupported test framework selected. Valid options are: " +
-          SUPPORTED_TEST_FRAMEWORKS.join(", ")
-      );
+        'Unsupported test framework selected. Valid options are: ' + SUPPORTED_TEST_FRAMEWORKS.join(', ')
+      )
     }
 
     /**
@@ -50,54 +44,56 @@ const COVERAGE_OUTPUT_FOLDER = "./coverage";
      * to use this action, feel free to open a feature request
      * in this repository 😉).
      **/
-    let testOutput = "";
+    let statistics = ''
 
-    if (input.framework === "jest") {
-      core.startGroup("Running Jest Tests...");
-      const JEST_PATH = "./node_modules/jest/bin/jest.js";
-      const JEST_FLAGS = "--no-cache --detectOpenHandles --coverage --json";
-      const RESULT_OUTPUT_FILE = `${COVERAGE_OUTPUT_FOLDER}/test-results.json`;
+    if (input.framework === 'jest') {
+      core.startGroup('Running Jest Tests...')
+      const JEST_PATH = './node_modules/jest/bin/jest.js'
+      const JEST_FLAGS = '--no-cache --detectOpenHandles --coverage --json'
+      const RESULT_OUTPUT_FILE = `${COVERAGE_OUTPUT_FOLDER}/test-results.json`
 
       await exec(`${JEST_PATH} ${JEST_FLAGS}`, undefined, {
         listeners: {
-          stdout: (data) => {
-            testOutput += data.toString();
-          },
-        },
-      });
+          stdout: data => {
+            statistics += data.toString()
+          }
+        }
+      })
 
-      fs.writeFileSync(RESULT_OUTPUT_FILE, testOutput);
+      fs.writeFileSync(RESULT_OUTPUT_FILE, statistics)
 
-      core.debug("Test output: ", testOutput);
-      core.endGroup();
+      core.debug('Test results: ', statistics)
+      core.endGroup()
     }
 
     /**
      * Calculate short version of commit hash
      * to use as the Cloudflare preview alias
      **/
-    core.startGroup("Calculating commit short hash...");
+    core.startGroup('Calculating commit short hash...')
 
-    let commitShortHash = github.context.sha.slice(0, 7);
+    let commitShortHash = github.context.sha.slice(0, 7)
 
-    core.debug("Calculated commit short hash: " + commitShortHash);
-    core.endGroup();
+    core.debug('Calculated commit short hash: ' + commitShortHash)
+    core.endGroup()
 
     /**
      * Now that we have the coverage report and
      * summary file generated, we will upload them to
      * Cloudflare Pages.
      **/
-    core.startGroup("Uploading to Cloudflare Pages...");
+    core.startGroup('Uploading to Cloudflare Pages...')
 
     await shellac`
     $ export CLOUDFLARE_API_TOKEN="${input.cloudflareApiToken}"
     $ export CLOUDFLARE_ACCOUNT_ID="${input.cloudflareAccountId}"
 
     $$ npx wrangler@2 pages publish "${COVERAGE_OUTPUT_FOLDER}" --project-name="${input.cloudflareProjectName}" --branch="${commitShortHash}"
-    `;
+    `
 
-    core.endGroup();
+    core.endGroup()
+
+    core.startGroup('Comment on available pull requests...')
 
     /**
      * Now we will create an instance of Octokit which
@@ -106,137 +102,92 @@ const COVERAGE_OUTPUT_FOLDER = "./coverage";
      * our requests. More information regarding Octokit here:
      * https://octokit.github.io/rest.js/v19
      **/
-    core.startGroup("Comment on available pull requests...");
-    const octokit = github.getOctokit(input.githubToken);
-    const UPLOAD_URL = `https://${commitShortHash}.${input.baseCloudflareDeploymentUrl}`;
-
-    // Get coverage summary with retries
-    const getCoverageSummary = async (
-      reportUrl,
-      { retryCount = 3, ignoreErrors = false } = {}
-    ) => {
-      try {
-        return await octokit.request(`${reportUrl}/coverage-summary.json`);
-      } catch (error) {
-        if (retryCount === 0) {
-          if (!ignoreErrors) throw error;
-        } else {
-          core.warning("Cloudflare pages request failed. Retrying...");
-          await new Promise((resolve) => setTimeout(resolve, 2000));
-          return await getCoverageSummary(reportUrl, {
-            retryCount: retryCount - 1,
-          });
-        }
-      }
-    };
-
-    // Get coverage summary with retries
-    const getTestResults = async (
-      reportUrl,
-      { retryCount = 3, ignoreErrors = false } = {}
-    ) => {
-      try {
-        return await octokit.request(`${reportUrl}/test-results.json`);
-      } catch (error) {
-        if (retryCount === 0) {
-          if (!ignoreErrors) throw error;
-        } else {
-          core.warning("Cloudflare pages request failed. Retrying...");
-          await new Promise((resolve) => setTimeout(resolve, 2000));
-          return await getCoverageSummary(reportUrl, {
-            retryCount: retryCount - 1,
-          });
-        }
-      }
-    };
+    const octokit = github.getOctokit(input.githubToken)
+    const UPLOAD_URL = `https://${commitShortHash}.${input.baseCloudflareDeploymentUrl}`
 
     const { data: pulls } = await octokit.rest.search.issuesAndPullRequests({
-      q: `is:pr state:open repo:${github.context.repo.owner}/${github.context.repo.repo} head:${input.branchName}`,
-    });
+      q: `is:pr state:open repo:${github.context.repo.owner}/${github.context.repo.repo} head:${input.branchName}`
+    })
 
-    core.info(`Pull requests available: ${JSON.stringify(pulls)}`);
+    core.info(`Pull requests available: ${JSON.stringify(pulls)}`)
 
     if (pulls.total_count > 0) {
-      pulls.items.forEach(async (pull) => {
+      pulls.items.forEach(async pull => {
         // Get the existing comments.
         const { data: pullRequest } = await octokit.rest.pulls.get({
           owner: github.context.repo.owner,
           repo: github.context.repo.repo,
-          pull_number: pull.number,
-        });
+          pull_number: pull.number
+        })
 
-        const shortBaseSha = pullRequest.base.sha.slice(0, 7);
+        const shortBaseSha = pullRequest.base.sha.slice(0, 7)
 
-        const headResult = await getCoverageSummary(UPLOAD_URL);
-        const headTotals = headResult.data.total;
+        // Read current head results from disk
+        const headResult = JSON.parse(fs.readFileSync(`${UPLOAD_URL}/coverage-summary.json`, 'utf8'))
+        const headTotals = headResult.data.total
         const headPercentages = [
           headTotals.lines.pct,
           headTotals.statements.pct,
           headTotals.functions.pct,
-          headTotals.branches.pct,
-        ];
-        const headAvgPercentage =
-          headPercentages.reduce((a, b) => a + b, 0) / headPercentages.length;
+          headTotals.branches.pct
+        ]
+        const headAvgPercentage
+          = headPercentages.reduce((a, b) => a + b, 0) / headPercentages.length
 
-        const baseResult = await getCoverageSummary(
-          `https://${shortBaseSha}.${input.baseCloudflareDeploymentUrl}`,
-          { retryCount: 0, ignoreErrors: true }
-        );
-        let baseTotals;
-        let basePercentages = [0, 0, 0, 0];
-        let baseAvgPercentage = 0;
+        // Retrieve base branch results from Cloudflare
+        // to compare with head
+        const baseResult = await getReport(
+          {
+            reportUrl: `https://${shortBaseSha}.${input.baseCloudflareDeploymentUrl}/coverage-summary.json`,
+            retryCount: 0,
+            ignoreErrors: true
+          }
+        )
+        let baseTotals
+        let basePercentages = [0, 0, 0, 0]
+        let baseAvgPercentage = 0
         if (baseResult) {
-          baseTotals = baseResult.data.total;
+          baseTotals = baseResult.data.total
           basePercentages = [
             baseTotals.lines.pct,
             baseTotals.statements.pct,
             baseTotals.functions.pct,
-            baseTotals.branches.pct,
-          ];
-          baseAvgPercentage =
-            basePercentages.reduce((a, b) => a + b, 0) / basePercentages.length;
+            baseTotals.branches.pct
+          ]
+          baseAvgPercentage
+            = basePercentages.reduce((a, b) => a + b, 0) / basePercentages.length
         }
 
-        let statistics = await getTestResults(UPLOAD_URL);
-        statistics = statistics.data;
-
-        const lastTestEndedAt =
-          statistics.testResults[statistics.testResults.length - 1].endTime;
-        const msDifference = lastTestEndedAt - statistics.startTime;
-        const secondsTaken = Math.floor(msDifference / 1000);
-        const minutesTaken = Math.floor(msDifference / 1000 / 60);
-        const hoursTaken = Math.floor(msDifference / 1000 / 60 / 60);
-
-        let timeTaken = "";
-        if (hoursTaken > 0) timeTaken = `${hoursTaken} hours, `;
-        if (minutesTaken > 0) timeTaken += `${minutesTaken} minutes and `;
-        if (secondsTaken > 0) timeTaken += `${secondsTaken} seconds`;
+        statistics = statistics.data
+        const startedAt = statistics.startTime
+        const endedAt = statistics.testResults[statistics.testResults.length - 1].endTime
+        const timeTaken = getTimeTaken(startedAt, endedAt)
 
         // Get the existing comments.
         const { data: comments } = await octokit.rest.issues.listComments({
           owner: github.context.repo.owner,
           repo: github.context.repo.repo,
-          issue_number: pullRequest.number,
-        });
+          issue_number: pullRequest.number
+        })
         const botComment = comments.find(
-          (comment) => comment.user.id === 41898282
-        );
+          comment => comment.user.id === 41898282
+        )
 
-        let coverageMessage;
+        let coverageMessage
         if (headAvgPercentage > baseAvgPercentage) {
           coverageMessage = `\n> Wooo 🎉, the tests are passing and the coverage percentage **increased** with this pull request, well done! 👏\n> ${
             pullRequest.base.ref
           }: **${Math.round(baseAvgPercentage, -1)}%** | ${
             input.branchName
-          }: **${Math.round(headAvgPercentage, -1)}%**`;
+          }: **${Math.round(headAvgPercentage, -1)}%**`
         } else if (headAvgPercentage === baseAvgPercentage) {
-          coverageMessage = `\n> Good job 👌, the tests are passing and the coverage percentage remained intact.`;
+          coverageMessage = '\n> Good job 👌, the tests are passing and the coverage percentage remained intact.'
         } else {
           coverageMessage = `\n> Tests are passing but the coverage percentage **is decreased** 😱 by this pull request, read coverage report below for more details.\n\n🔻 ${
             pullRequest.base.ref
           }: **${Math.round(baseAvgPercentage, -1)}%** | ${
             input.branchName
-          }: **${Math.round(headAvgPercentage, -1)}%** 🔻`;
+          }: **${Math.round(headAvgPercentage, -1)}%** 🔻`
         }
 
         let coverageSummaryTable = `
@@ -244,58 +195,58 @@ const COVERAGE_OUTPUT_FOLDER = "./coverage";
 @@                             Coverage Summary                          @@
    -----------------------------------------------------------------------
   |   Category   |  Master Branch  |  Current Branch  |  Covered / Total  |
-  | ------------ | --------------- | ---------------- | ----------------- |`;
+  | ------------ | --------------- | ---------------- | ----------------- |`
 
         coverageSummaryTable += `\n${
           !baseResult || headTotals.statements.pct > baseTotals.statements.pct
-            ? "+"
+            ? '+'
             : baseTotals.statements.pct === headTotals.statements.pct
-            ? " "
-            : "-"
-        } `;
+              ? ' '
+              : '-'
+        } `
         coverageSummaryTable += `| Statements   |     ${
-          baseResult ? baseTotals.statements.pct.toString() + "%" : "   -  "
+          baseResult ? baseTotals.statements.pct.toString() + '%' : '   -  '
         }      |      ${headTotals.statements.pct}%      |       ${
           headTotals.statements.covered
-        }/${headTotals.statements.total}     |`;
+        }/${headTotals.statements.total}     |`
         coverageSummaryTable += `\n${
           !baseResult || headTotals.branches.pct > baseTotals.branches.pct
-            ? "+"
+            ? '+'
             : baseTotals.branches.pct === headTotals.branches.pct
-            ? " "
-            : "-"
-        } `;
+              ? ' '
+              : '-'
+        } `
         coverageSummaryTable += `| Branches     |     ${
-          baseResult ? baseTotals.branches.pct.toString() + "%" : "   -  "
+          baseResult ? baseTotals.branches.pct.toString() + '%' : '   -  '
         }      |      ${headTotals.branches.pct}%      |       ${
           headTotals.branches.covered
-        }/${headTotals.branches.total}     |`;
+        }/${headTotals.branches.total}     |`
         coverageSummaryTable += `\n${
           !baseResult || headTotals.functions.pct > baseTotals.functions.pct
-            ? "+"
+            ? '+'
             : baseTotals.functions.pct === headTotals.functions.pct
-            ? " "
-            : "-"
-        } `;
+              ? ' '
+              : '-'
+        } `
         coverageSummaryTable += `| Functions    |     ${
-          baseResult ? baseTotals.functions.pct.toString() + "%" : "   -  "
+          baseResult ? baseTotals.functions.pct.toString() + '%' : '   -  '
         }      |      ${headTotals.functions.pct}%      |       ${
           headTotals.functions.covered
-        }/${headTotals.functions.total}     |`;
+        }/${headTotals.functions.total}     |`
         coverageSummaryTable += `\n${
           !baseResult || headTotals.lines.pct > baseTotals.lines.pct
-            ? "+"
+            ? '+'
             : baseTotals.lines.pct === headTotals.lines.pct
-            ? " "
-            : "-"
-        } `;
+              ? ' '
+              : '-'
+        } `
         coverageSummaryTable += `| Lines        |     ${
-          baseResult ? baseTotals.lines.pct.toString() + "%" : "   -  "
+          baseResult ? baseTotals.lines.pct.toString() + '%' : '   -  '
         }      |      ${headTotals.lines.pct}%      |       ${
           headTotals.lines.covered
-        }/${headTotals.lines.total}     |`;
+        }/${headTotals.lines.total}     |`
 
-        coverageSummaryTable += `\n  -------------------------------------------------------------------------\n\`\`\``;
+        coverageSummaryTable += '\n  -------------------------------------------------------------------------\n```'
 
         const commentBody = `## 🔖 Coverage Report
 ${coverageMessage}
@@ -313,28 +264,63 @@ ${coverageSummaryTable}
 
 > Coverage data is based on head **${input.branchName}** (\`${commitShortHash}\`) compared to base **${pullRequest.base.ref}** (\`${shortBaseSha}\`).
 
-[View full coverage report 🔗](${UPLOAD_URL})`;
+[View full coverage report 🔗](${UPLOAD_URL})`
 
         if (botComment) {
           await octokit.rest.issues.updateComment({
             owner: github.context.repo.owner,
             repo: github.context.repo.repo,
             comment_id: botComment.id,
-            body: commentBody,
-          });
+            body: commentBody
+          })
         } else {
           await octokit.rest.issues.createComment({
             issue_number: pullRequest.number,
             owner: github.context.repo.owner,
             repo: github.context.repo.repo,
-            body: commentBody,
-          });
+            body: commentBody
+          })
         }
-      });
+      })
     }
 
-    core.endGroup();
+    core.endGroup()
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
-})();
+})()
+
+const getReport = async ({ reportUrl, retryCount = 3, ignoreErrors = false } = {}) => {
+  try {
+    const http = new HttpClient()
+    const res = await http.get(reportUrl)
+    const body = await res.readBody()
+
+    return JSON.parse(body)
+  } catch (error) {
+    if (retryCount === 0) {
+      if (!ignoreErrors) throw error
+    } else {
+      core.warning('Cloudflare pages request failed. Retrying...')
+      await new Promise(resolve => setTimeout(resolve, 2000))
+      return await getReport({
+        reportUrl,
+        retryCount: retryCount - 1
+      })
+    }
+  }
+}
+
+const getTimeTaken = (startedAt, endedAt) => {
+  const msDifference = endedAt - startedAt
+  const secondsTaken = Math.floor(msDifference / 1000)
+  const minutesTaken = Math.floor(msDifference / 1000 / 60)
+  const hoursTaken = Math.floor(msDifference / 1000 / 60 / 60)
+
+  let timeTaken = ''
+  if (hoursTaken > 0) timeTaken = `${hoursTaken} hours, `
+  if (minutesTaken > 0) timeTaken += `${minutesTaken} minutes and `
+  if (secondsTaken > 0) timeTaken += `${secondsTaken} seconds`
+
+  return timeTaken
+}
