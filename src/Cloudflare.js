1ccc7e52be304260b5d91618cfd2fbd2e3cedbc1
Carlos Pereira <carlos.dev.pereira@gmail.com>
2022-10-08 15:22:12 +0100

    fix: unterminated quote string

--- src/Cloudflare.js
+++ src/Cloudflare.js
@@ -11,7 +11,7 @@ class Cloudflare {
   }
 
   publish(commitSha) {
-    execSync(`npx wrangler@2 pages publish "${COVERAGE_OUTPUT_FOLDER}" --project-name="${this.projectName}" --branch="${commitSha}`, {
+    execSync(`npx wrangler@2 pages publish "${COVERAGE_OUTPUT_FOLDER}" --project-name="${this.projectName}" --branch="${commitSha}"`, {
       env: {
         CLOUDFLARE_API_TOKEN: this.apiToken,
         CLOUDFLARE_ACCOUNT_ID: this.accountId
