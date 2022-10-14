c82ed60e9fefd80359fc3929ecbad6dd051c80e8
Carlos Pereira <carlos.dev.pereira@gmail.com>
2022-10-14 18:10:52 +0100

    test: cloudflare class

--- tests/Cloudflare.spec.js
+++ tests/Cloudflare.spec.js
@@ -14,29 +14,42 @@ describe('Cloudflare', () => {
     projectName: 'project-name',
   });
 
-  it('publishes to cloudflare', async () => {
-    await cloudflareInstance.publish('1234');
+  it('returns an instance of a Commit', () => {
+    expect(cloudflareInstance.apiToken).toBe('api-token');
+    expect(cloudflareInstance.accountId).toBe('account-id');
+    expect(cloudflareInstance.projectName).toBe('project-name');
+    expect(cloudflareInstance.baseUrl).toBe('project-url.pages.dev');
 
-    expect(shellac).toHaveBeenCalledWith(
-      [
-        '\n    $ export CLOUDFLARE_API_TOKEN="',
-        '"\n    $ export CLOUDFLARE_ACCOUNT_ID="',
-        '"\n    $$ npx wrangler@2 pages publish "',
-        '" --project-name="',
-        '" --branch="',
-        '"',
-      ],
-      'api-token',
-      'account-id',
-      './coverage',
-      'project-name',
-      '1234'
-    );
+    expect(typeof cloudflareInstance.publish).toBe('function');
   });
 
-  it('returns the report url', async () => {
-    const reportUrl = await cloudflareInstance.publish('1234');
+  describe('publish method', () => {
+    let reportUrl;
 
-    expect(reportUrl).toBe('https://1234.project-url.pages.dev');
+    beforeAll(async () => {
+      reportUrl = await cloudflareInstance.publish('1234');
+    });
+
+    it('publishes to cloudflare', () => {
+      expect(shellac).toHaveBeenCalledWith(
+        [
+          '\n    $ export CLOUDFLARE_API_TOKEN="',
+          '"\n    $ export CLOUDFLARE_ACCOUNT_ID="',
+          '"\n    $$ npx wrangler@2 pages publish "',
+          '" --project-name="',
+          '" --branch="',
+          '"',
+        ],
+        'api-token',
+        'account-id',
+        './coverage',
+        'project-name',
+        '1234'
+      );
+    });
+
+    it('returns the report url', () => {
+      expect(reportUrl).toBe('https://1234.project-url.pages.dev');
+    });
   });
 });
