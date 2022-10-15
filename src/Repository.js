6168dc78d583154c48bec1b2cd228f30e56373e2
Carlos Pereira <carlos.dev.pereira@gmail.com>
2022-10-15 11:55:55 +0100

    chore: print action progress to github console

--- src/Repository.js
+++ src/Repository.js
@@ -1,3 +1,4 @@
+const core = require('@actions/core');
 const github = require('@actions/github');
 const Framework = require('./Framework');
 const { BuildCommentBody } = require('./utils/buildComment');
@@ -41,37 +42,60 @@ class Repository {
   }
 
   async addCommitComment(commitSha, comment) {
-    await this.github.rest.repos.createCommitComment({
-      owner: this.owner,
-      repo: this.name,
-      commit_sha: commitSha,
-      body: comment
-    });
+    const commitComment = await this.getCommitComment(commitSha);
+
+    if (commitComment) {
+      core.info('Updating existing commit comment `' + commitComment.id + '`...');
+
+      await this.github.rest.repos.updateCommitComment({
+        owner: this.owner,
+        repo: this.name,
+        comment_id: commitComment.id,
+        body: comment
+      });
+
+      core.info('Comment `' + commitComment.id + '` updated!');
+    }
+    else {
+      core.info('Creating a new commit comment...');
+
+      const createdComment = await this.github.rest.repos.createCommitComment({
+        owner: this.owner,
+        repo: this.name,
+        commit_sha: commitSha,
+        body: comment
+      });
+
+      core.info('Added comment `' + createdComment.id + '` to commit `' + commitSha + '`!');
+    }
   }
 
   async getCommitComment(commitSha) {
+    core.info('Retrieving comments for commit `' + commitSha + '`...');
+
     const comments = await this.github.rest.repos.listCommentsForCommit({
       owner: this.owner,
       repo: this.name,
       commit_sha: commitSha
     });
 
-    return comments.data[0].body;
+    if (!comments.data.length) {
+      core.info('Commit `' + commitSha + '` doesnt have comments yet!');
+      return null;
+    }
+
+    core.info('Found comments for commit `' + commitSha + '`!');
+    return comments.data[0];
   }
 
   async commentPullRequest(pullRequest, fullReportUrl) {
-    const { data: comments } = await this.github.rest.issues.listComments({
-      issue_number: pullRequest.number,
-      owner: this.owner,
-      repo: this.name,
-    });
-    const botComment = comments.find((comment) => {
-      return comment.user.id === 41_898_282;
-    });
+    const results = await Promise.all([
+      this.getCommitComment(pullRequest.headBranchSha),
+      this.getCommitComment(pullRequest.baseBranchSha)
+    ]);
 
-    // TODO: This can be parallelized
-    const headResult = JSON.parse(await this.getCommitComment(pullRequest.headBranchSha));
-    const baseResult = JSON.parse(await this.getCommitComment(pullRequest.baseBranchSha));
+    const headResult = JSON.parse(results[0].body);
+    const baseResult = JSON.parse(results[1].body);
 
     const commentBody = await BuildCommentBody({
       baseAvgPercentage: TotalPercentagesAverage(baseResult),
@@ -87,21 +111,46 @@ class Repository {
       testResults: headResult.stats,
     });
 
+    await this.addPullRequestComment(pullRequest.number, commentBody);
+  }
+
+  async addPullRequestComment(pullRequestNumber, comment) {
+    const comments = await this.getPullRequestComments(pullRequestNumber);
+    const botComment = comments.find((comment) => {
+      return comment.user.id === 41_898_282;
+    });
+
     if (botComment) {
+      core.info('Updating comment for pull request #`' + pullRequestNumber + '`...');
+
       await this.github.rest.issues.updateComment({
-        body: commentBody,
+        body: comment,
         comment_id: botComment.id,
         owner: this.owner,
         repo: this.name,
       });
     } else {
+      core.info('Creating a new comment for pull request #`' + pullRequestNumber + '`...');
+
       await this.github.rest.issues.createComment({
-        body: commentBody,
-        issue_number: pullRequest.number,
+        body: comment,
+        issue_number: pullRequestNumber,
         owner: this.owner,
         repo: this.name,
       });
     }
+  }
+
+  async getPullRequestComments(pullRequestNumber) {
+    core.info('Searching for comments on pull request #`' + pullRequestNumber + '`...');
+
+    const { data: comments } = await this.github.rest.issues.listComments({
+      issue_number: pullRequestNumber,
+      owner: this.owner,
+      repo: this.name,
+    });
+
+    return comments;
   }
 }
 
