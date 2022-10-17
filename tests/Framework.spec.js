b508864c563a29b358db203d800b74e411e439a4
Carlos Pereira <carlos.dev.pereira@gmail.com>
2022-10-17 22:31:26 +0100

    test: framework instance

--- tests/Framework.spec.js
+++ tests/Framework.spec.js
@@ -1,11 +1,17 @@
+const fs = require('node:fs');
+const exec = require('@actions/exec');
+
+const testResults = require('@tests/fixtures/test-stats.json');
 const coverageSummaryFixture = require('@tests/fixtures/coverage-summary.json');
 
-describe('Framework -> Happy path', () => {
-  const fs = require('node:fs');
-  jest.spyOn(fs, 'readFileSync').mockImplementation(() => {
-    return coverageSummaryFixture;
-  });
+jest.spyOn(fs, 'readFileSync').mockImplementation(() => JSON.stringify(coverageSummaryFixture));
+jest.spyOn(exec, 'exec').mockImplementation((commandToExecute, args, options) => {
+  if (options && options.listeners && options.listeners.stdout) {
+    options.listeners.stdout(JSON.stringify(testResults));
+  }
+});
 
+describe('Framework -> Happy path', () => {
   const Framework = require('@/Framework');
   const testingFrameworkInstance = new Framework('jest');
 
@@ -14,5 +20,25 @@ describe('Framework -> Happy path', () => {
     expect(testingFrameworkInstance.testResults).toBe(null);
 
     expect(typeof testingFrameworkInstance.runTests).toBe('function');
+  });
+
+  describe('runTests method', () => {
+    let results;
+    beforeAll(async () => {
+      results = await testingFrameworkInstance.runTests();
+    });
+
+    it('executes the jest command with correct options', () => {
+      expect(exec.exec).toHaveBeenCalledWith('node --experimental-vm-modules ./node_modules/jest/bin/jest.js --no-cache --detectOpenHandles --coverage --json', undefined, expect.anything());
+    });
+
+    it('reads the coverage summary and returns the test results', () => {
+      expect(fs.readFileSync).toHaveBeenCalledWith('./coverage/coverage-summary.json', 'utf-8');
+
+      expect(results).toStrictEqual({
+        stats: testResults,
+        summary: coverageSummaryFixture
+      });
+    });
   });
 });
